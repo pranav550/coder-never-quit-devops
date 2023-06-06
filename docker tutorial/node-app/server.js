bbac3be4bf17f3da4b5127ed2761359e8d5c7731
pranav550 <pranav550@gmail.com>
2023-06-06 20:07:21 +0530

    devops with inital kubernetes

---
+++ docker tutorial/node-app/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/",(req, res)=>{
+    res.send('<h1>docker is awesome3</h1>');
+})
+app.get("/about",(req, res)=>{
+    res.send(process.env.API_KEY);
+})
+
+app.listen(5000,()=>{
+    console.log('server running  on 5000')
+})
